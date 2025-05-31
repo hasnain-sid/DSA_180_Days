96403a8508d426c15cc945a2fba3e7977877bf7c
hasnain-sid <129553722+hasnain-sid@users.noreply.github.com>
2025-05-31 18:08:11 +0530

    [853] [Time Beats: 77.15%] [Memory Beats: 82.76%] - LeetPush

--- Day_35/Car-Fleet.py
+++ Day_35/Car-Fleet.py
@@ -1,9 +1,11 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        ans = prev = 0
+        ans = 0
+        st = []
+        st.append(-1)
         for pp, ss in sorted(zip(position, speed), reverse=True): 
             tt = (target - pp)/ss # time to arrive at target 
-            if prev < tt: 
+            if st and st[-1] < tt: 
                 ans += 1
-                prev = tt
+                st.append(tt)
         return ans 
