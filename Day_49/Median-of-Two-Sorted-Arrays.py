baef26aa55b508eea0d08fa2e1a480d5156b88eb
hasnain-sid <129553722+hasnain-sid@users.noreply.github.com>
2025-06-30 20:18:10 +0530

    [4] [Time Beats: 57.95%] [Memory Beats: 83.01%] - LeetPush

--- Day_49/Median-of-Two-Sorted-Arrays.py
+++ Day_49/Median-of-Two-Sorted-Arrays.py
@@ -1,19 +1,29 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1, nums2):
-        # Merge the arrays into a single sorted array.
-        merged = nums1 + nums2
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1)>len(nums2):           #Need to do bs on shorter one to prevent out of bounds on other one
+            nums1,nums2=nums2,nums1
+        
+        n1,n2=len(nums1),len(nums2)
+        low,high=0,n1
+        half=(n1+n2+1)//2       #Why +1??
 
-        # Sort the merged array.
-        merged.sort()
+        while(low<=high):           #Why <=? cuz finding exact target val??
+            mid1=(low+high)//2          #partition cuts
+            mid2=half-mid1
 
-        # Calculate the total number of elements in the merged array.
-        total = len(merged)
+            l1=nums1[mid1-1] if mid1>=1 else float('-inf') # Left and right values around the cuts
+            l2=nums2[mid2-1] if mid2>=1 else float('-inf')      
+            r1=nums1[mid1] if mid1<n1 else float('inf')    #since it has to be in increasing order
+            r2=nums2[mid2] if mid2<n2 else float('inf')
 
-        if total % 2 == 1:
-            # If the total number of elements is odd, return the middle element as the median.
-            return float(merged[total // 2])
-        else:
-            # If the total number of elements is even, calculate the average of the two middle elements as the median.
-            middle1 = merged[total // 2 - 1]
-            middle2 = merged[total // 2]
-            return (float(middle1) + float(middle2)) / 2.0
+            if l1<=r2 and l2<=r1:           #Valid order, so find median
+                if (n1+n2)%2==0:            #Even, so median is sum of middle elements
+                    return (max(l1,l2)+min(r1,r2))/2
+                else:
+                    return max(l1,l2)       #Since left has an extra element
+            elif l1>r2:         #Move left to remove the bigger element from consideration
+                high=mid1-1
+            else:                   #Condition: l2>r1, move right to include smaller element
+                low=mid1+1
+
+
