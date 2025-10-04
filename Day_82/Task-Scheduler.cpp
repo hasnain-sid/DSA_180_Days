da6fe155b4c90359fd4ee92d7ff932e69b42fdee
Hasnain Akhtar Siddique <129553722+hasnain-sid@users.noreply.github.com>
2025-10-04 09:18:53 +0530

    [621] [Time Beats: 9.51%] [Memory Beats: 10.25%] - LeetPush

--- Day_82/Task-Scheduler.cpp
+++ Day_82/Task-Scheduler.cpp
@@ -2,37 +2,31 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         priority_queue<int> pq;
-        vector<int>mp(26,0);
-
-        for(char i:tasks){
-            mp[i-'A']++;  // count the number of times a task needs to be done
-        }   
-        for(int i=0;i<26;++i){
-            if(mp[i]) 
-            pq.push(mp[i]);
+        map<char,int> mp;
+        for(auto it:tasks){
+            mp[it]++;
         }
-
-        int time=0; // stores the total time taken 
+        int ans = 0;
+        for(auto it:mp){
+            pq.push(it.second);
+        }
         while(!pq.empty()){
-            vector<int>remain;
-            int cycle=n+1;  // n+1 is the CPU cycle length, if n is the cooldown period then after a task A there will be n more tasks. Hence n+1.
-
-            while(cycle and !pq.empty()){
-                int max_freq=pq.top(); // the task at the top should be first assigned the CPU as it has highest frequency
+            int limit = n+1,done = 0;
+            vector<int> rem;
+            while(limit and !pq.empty()){
+                int maxm = pq.top();
                 pq.pop();
-                if(max_freq>1){ // task with more than one occurrence, the next occurrence will be done in the next cycle 
-                    remain.push_back(max_freq-1); // add it to remaining task list
+                done++;
+                if(maxm-1>0){
+                    rem.push_back(maxm-1);
                 }
-                ++time; 
-                --cycle; 
+                limit--;
             }
-
-            for(int count:remain){
-                pq.push(count); 
-            }
-            if(pq.empty())break; // if the priority queue is empty than all tasks are completed and we don't need to include the idle time
-            time+=cycle; // this counts the idle time 
+            for(auto it:rem) pq.push(it);     
+            if(pq.empty())ans+=done;
+            else ans+= n+1;   
+             
         }
-        return time;
+        return ans;
     }
 };
